336747541318b4ba069b43c2f2bfcd349eabefa0
Rodrigo <zoccorodrigo@gmail.com>
2023-01-31 19:45:35 -0300

    Agrega funcionalidad de tapar las fichas
    Luego de pintar las fichas de forma aleatoria, es necesario "tapar" las fichas.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-
-
 const $BOTON_JUGAR = document.querySelector("#jugar");
 document.querySelector("#tablero-juego").classList.add("oculto");
 
 $BOTON_JUGAR.onclick = function(){
-
     document.querySelector("#panel-inicio").classList.add("oculto");
     document.querySelector("#tablero-juego").classList.remove("oculto");
     comenzarJuego();
@@ -14,6 +11,7 @@ function comenzarJuego(){
     let numeroMovimientos = 0;
     const PATRON_FICHAS = mezclarFichas();
     pintarFichas(PATRON_FICHAS);
+    ocultarFichas();
 }
 
 function mezclarFichas(){
@@ -36,12 +34,19 @@ function pintarFichas(arrayFichas){
     });
 }
 
+function ocultarFichas(){
+    const FICHAS = document.querySelectorAll(".ficha");
+    FICHAS.forEach(function(FICHA){
+        FICHA.classList.add("tapada");
+    });
+}
+
 /*
 XnumeroMovimientos = 0;
 XpatronFichas = [];
 XFunción mezclarFichas() {se introducen todas las id de colores, se van elegiendo de forma aleatoria y rellenan el patronFichas}
 XFunción pintarFichas(patronFichas){ForEach ".ficha" se le agrega el patronFicha[ficha] como classList.add}//Esto pintará los casilleros
-Función ocultarFichas(){ForEach ".ficha" classlist.add("Tapada")}//Tapada en CSS = color: black !important;
+XFunción ocultarFichas(){ForEach ".ficha" classlist.add("Tapada")}//Tapada en CSS = color: black !important;
 Función desbloquearInput(){ForEach ".ficha" //Si la clickeo, le elimino classlist.remove("Tapada")}//Se verá el color.
 //Comparación de colores: 
 */
