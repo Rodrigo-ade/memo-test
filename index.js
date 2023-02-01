3e72087f7152863aa27d345b305e0cf711e70cef
Rodrigo <zoccorodrigo@gmail.com>
2023-02-01 09:34:24 -0300

    Actualiza funcionalidad
    El juego esta completo, falta completar la victoria.

--- index.js
+++ index.js
@@ -5,10 +5,79 @@ $BOTON_JUGAR.onclick = function(){
     document.querySelector("#panel-inicio").classList.add("oculto");
     document.querySelector("#tablero-juego").classList.remove("oculto");
     comenzarJuego();
+    desbloquearInputFichas();
+}
+
+function desbloquearInputFichas(){
+    let $fichas = document.querySelectorAll(".ficha");
+    $fichas.forEach(function(ficha){
+        ficha.onclick = manejarInputUsuario;
+    });
+}
+
+function bloquearInputFichas(){
+    let $fichas = document.querySelectorAll(".ficha");
+    $fichas.forEach(function(ficha){
+        ficha.onclick = function(){
+
+        };
+    });
+}
+
+let fichaA = null;
+let cantidadMovimientos = 0;
+let paresEncontrados = 0;
+function manejarInputUsuario(evento){
+    if(fichaA === null){
+        fichaA = evento.target;
+        fichaA.classList.remove("tapada");
+    }
+
+    if(fichaA !== evento.target){
+        const DELAY_COMPROBACION = 500;
+        evento.target.classList.remove("tapada");
+        bloquearInputFichas();
+        if(fichaA.classList[2] === evento.target.classList[2]){
+            //Fichas son iguales
+            paresEncontrados ++;
+            setTimeout(function(){
+                fichaA.classList.add("encontrada");
+                evento.target.classList.add("encontrada");
+            },DELAY_COMPROBACION);
+            
+        }else{
+            //Fichas son distintas
+            setTimeout(function(){
+                fichaA.classList.add("tapada");
+                evento.target.classList.add("tapada");
+            },DELAY_COMPROBACION);
+        }
+
+        cantidadMovimientos++;
+        setTimeout(function(){
+            fichaA = null;
+            evento.target = null;
+            desbloquearInputFichas();
+            actualizarNumeroMovimientos();
+            console.log(paresEncontrados);
+            comprobarVictoria();
+        },DELAY_COMPROBACION);
+    }
+
+}
+
+const CANTIDAD_PARES = document.querySelectorAll(".ficha").length / 2;
+function comprobarVictoria(){
+    if(paresEncontrados === CANTIDAD_PARES ){
+        //COMPLETAR
+    }
+}
+
+function actualizarNumeroMovimientos(){
+    document.querySelector("#cantidad-movimientos").textContent = cantidadMovimientos;
 }
 
 function comenzarJuego(){
-    let numeroMovimientos = 0;
     const PATRON_FICHAS = mezclarFichas();
     pintarFichas(PATRON_FICHAS);
     ocultarFichas();
@@ -40,13 +109,3 @@ function ocultarFichas(){
         FICHA.classList.add("tapada");
     });
 }
-
-/*
-XnumeroMovimientos = 0;
-XpatronFichas = [];
-XFunción mezclarFichas() {se introducen todas las id de colores, se van elegiendo de forma aleatoria y rellenan el patronFichas}
-XFunción pintarFichas(patronFichas){ForEach ".ficha" se le agrega el patronFicha[ficha] como classList.add}//Esto pintará los casilleros
-XFunción ocultarFichas(){ForEach ".ficha" classlist.add("Tapada")}//Tapada en CSS = color: black !important;
-Función desbloquearInput(){ForEach ".ficha" //Si la clickeo, le elimino classlist.remove("Tapada")}//Se verá el color.
-//Comparación de colores: 
-*/
