30751b360fc90b12bc5444721c5afe72851d4791
Rodrigo <zoccorodrigo@gmail.com>
2023-02-17 08:04:10 -0300

    Completa los test de cypress
    La aplicación es testeada

--- cypress/e2e/memo-test-prueba.cy.js
+++ cypress/e2e/memo-test-prueba.cy.js
@@ -6,7 +6,6 @@ context("MemoTest", () => {
     cy.visit(URL);
   })
 
-
   describe("Pruebas previas a jugar", () => {
     it("Verifica que el panel de inicio sea visible", () => {
       cy.get("#panel-inicio").should("be.visible");
@@ -37,20 +36,43 @@ context("MemoTest", () => {
   });
   
   describe("Pruebas de resolución del juego", () => {
+    let fichas;
     let paresDeFichasOrdenados;
+
     it("Verifica que se tapen las fichas al elegir 2 diferentes", () => {
-      
+     fichas = obtenerFichas();
+     cy.get(fichas).then((fichas) =>{
+      paresDeFichasOrdenados = ordernarFichas(fichas);
+     }).then(() => {
+        cy.get(paresDeFichasOrdenados[0][0]).click();
+        cy.get(paresDeFichasOrdenados[1][0]).click();
+        cy.get(paresDeFichasOrdenados[0][0]).should("have.class", "tapada");
+        cy.get(paresDeFichasOrdenados[1][0]).should("have.class", "tapada");
+      });
     });
 
     it("Verifica que se oculten las fichas si se eligen 2 iguales", () => {
-
+      cy.get(paresDeFichasOrdenados[0][0]).click();
+      cy.get(paresDeFichasOrdenados[0][1]).click();
+      cy.get(paresDeFichasOrdenados[0][0]).should("have.class", "encontrada");
+      cy.get(paresDeFichasOrdenados[0][1]).should("have.class", "encontrada");
     });
 
-    it("Resuelve el juego y comprueba que se vea la notificación al ganar", () => {
-      
+    it("Resuelve el juego y comprueba que el tablero se oculte al ganar", () => {
+      paresDeFichasOrdenados.forEach(($parDeFichasOrdenado,index) => {
+        if(index > 0){
+          cy.get($parDeFichasOrdenado[0]).click();
+          cy.get($parDeFichasOrdenado[1]).click();
+          cy.wait(600);
+        }
+      });
+      cy.get("#tablero-juego").should("be.hidden");
+    });
+
+    it("Verifica que al ganar se vea ¡Ganaste! con el nº de movimientos", () => {
+      cy.get("#titulo").contains(`Has ganado en ${(fichas.length / 2 + 1)} movimientos! presiona jugar para volver a jugar.`);
     });
   });
-  
 });
 
 function obtenerFichas(){
@@ -62,4 +84,19 @@ function obtenerFichas(){
   })
 
   return fichas;
+}
+
+function ordernarFichas($fichas){
+  let paresFichasOrdenados = {};
+  $fichas.each((indice,$ficha) => {
+    let colorFicha = $ficha.classList[2];
+    if(paresFichasOrdenados[colorFicha]){
+      paresFichasOrdenados[colorFicha].push($ficha);
+    }
+    else{
+      paresFichasOrdenados[colorFicha] = [$ficha];
+    }
+  });
+
+  return Object.values(paresFichasOrdenados);
 }
